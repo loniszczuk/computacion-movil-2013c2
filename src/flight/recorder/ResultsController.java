6cf4c019df74a2d7149fe15e72f5ff7871cc2e40
nrabazano <nrabazano@gmail.com>
2013-10-09 08:14:23 -0300

    vista de fb, solo boton

--- src/flight/recorder/ResultsController.java
+++ src/flight/recorder/ResultsController.java
@@ -11,19 +11,6 @@ public class ResultsController implements OnClickListener {
 	private final ResultsView view;
 	private final ResultsModel model;
 	
-	/*facebook
-	// application id from facebook.com/developers
-	public static final String APP_ID = "536274329783175";
-	// log tag for any log.x statements
-	public static final String TAG = "FACEBOOK CONNECT";
-	// permissions array
-	private static final String[] PERMS = new String[] { "user_events" };
-	// facebook vars
-	private Facebook mFacebook;
-	private AsyncFacebookRunner mAsyncRunner;
-	// id text view
-	private TextView mText;
- */
 	
 	public ResultsController(ResultsModel m, ResultsView v) {
 		this.model = m;
@@ -34,6 +21,10 @@ public class ResultsController implements OnClickListener {
 	public void onClick(View view) {
 		if (this.view.getButtonMap() == view) {
 			this.view.showMap();
+		}else{
+			if (this.view.getButtonFb() == view){
+				this.view.connectFb();
+			}
 		}
 	}
 	
