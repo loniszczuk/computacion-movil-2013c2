281a07d94ce7f571e5841b3e1bb562148cd45202
iarah <ohkernelpanic@gmail.com>
2013-10-30 15:12:31 -0300

    Hecho el logging the facebook

--- src/flight/recorder/ResultsActivity.java
+++ src/flight/recorder/ResultsActivity.java
@@ -14,7 +14,6 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
